3040f0df195e586de886a377a35e21bcc8b36f18
Simon <simonwatson176@gmail.com>
2021-07-30 17:36:05 +0100

    url, json link added

--- src/containers/BeerContainer.js
+++ src/containers/BeerContainer.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import BeerList from '../components/BeerList';
 import BeerSelector from '../components/BeerSelector';
 
 const BeerContainer = () => {
+    const [beers, setBeers] = useState([]);
 
-    return (
+    const getBeers = function (){
+        
+         fetch('https://api.punkapi.com/v2/beers')
+        .then(res => res.json())
+        .then(beers => setBeers(beers))
+     }
+
+
+      return (
         <>
         <p>im a container</p>
   
